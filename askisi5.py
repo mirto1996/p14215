dfd8c74b29eda3ad1206e2bab0e2ed3d3d4a194f
mirto1996 <myrtover@hotmail.com>
2016-03-11 00:19:18 +0200

    first commit

---
+++ askisi5.py
@@ -0,0 +1,25 @@
+﻿d= int(input("enter date:"))
+m= int(input("enter month:"))
+y= int(input("enter year:"))
+ 
+if m<3:
+	m=m+12
+	y=y-1
+a=(2*m)+(6*(m+1)/10)
+b=y+(y/4)+(y/400)-(y/100)
+c=d+a+b+1
+f=c%7
+if f==0:
+	print "sunday"
+elif f==1:
+	print "monday"
+elif f==2:
+	print "tuesday"
+elif f==3:
+	print "wednesday"
+elif f==4:
+	print "thursday"
+elif f==5:
+	print "friday"
+else:
+	print "saturday"
